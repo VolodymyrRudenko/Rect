ddfa961588b34c7868f103a2a0e1769927dcd7e2
VolodymyrRudenko <59016863+VolodymyrRudenko@users.noreply.github.com>
2020-01-10 18:02:17 +0300

    New Class Rect

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+
+        class Main {
+            public static void main(String[] args) {
+                Rect rect = new Rect();
+                rect.Ploscha(7, 9);
+                rect.perumeter(7, 9);
+
+            }
+        }
